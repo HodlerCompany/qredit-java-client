52f804646efef4a45251e79ddb25989a0f82f831
Eric <opabinia@126.com>
2018-11-09 11:44:35 +0800

    fix compile fails

--- src/main/java/qredit_java_client/QreditNetworkSettings.java
+++ src/main/java/qredit_java_client/QreditNetworkSettings.java
@@ -1,16 +1,16 @@
 package qredit_java_client;
 
+import java.util.List;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
-public class ArkNetworkSettings {
-    private String scheme;
-    private List<ArkNetworkPeerSettings> seedPeers;
-    private List<ArkNetworkPeerSettings> trustedPeers;
-    private String netHash;
-    private Integer pubKeyHash = 58;
-    private String epoch = "2017-03-21 13:00:00";
-    private String version;
+public class QreditNetworkSettings {
+
+  private String scheme;
+  private List<QreditNetworkPeerSettings> seedPeers;
+  private List<QreditNetworkPeerSettings> trustedPeers;
+  private String netHash;
+  private Integer pubKeyHash = 58;
+  private String epoch = "2017-03-21 13:00:00";
+  private String version;
 }
